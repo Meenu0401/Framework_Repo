c4b52e43e5ed0a0f18802f1074ee2f13d02aeb4d
Meenu3112 <gupta_meenu77@yahoo.com>
2024-07-30 17:09:25 +0530

    fgg

---
+++ Practice/src/test/java/P1/Demo1Test.java
@@ -0,0 +1,11 @@
+package P1;
+
+import org.testng.annotations.Test;
+
+public class Demo1Test{
+
+	@Test
+	public void frameTest() {
+		System.out.println("Gfffgfgf tbbh");
+	}
+}
---
+++ Practice/src/test/java/P2/Demo2Test.java
@@ -0,0 +1,12 @@
+package P2;
+
+import org.testng.annotations.Test;
+
+public class Demo2Test {
+
+	@Test
+	public void framesTest() {
+		System.out.println("---executing frames----");
+	}
+	
+}
